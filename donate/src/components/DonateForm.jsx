26c96ccc36224da76b489bd5bf38292c9e7bf3ca
Vadim Kovalyov <vadimkovalyov06@gmail.com>
2026-04-06 14:04:00 +0300

    Updated logic for sending donate

--- donate/src/components/DonateForm.jsx
+++ donate/src/components/DonateForm.jsx
@@ -5,22 +5,38 @@ export default function DonateForm() {
   const [amount, setAmount] = useState("");
   const [message, setMessage] = useState("");
   const [link, setLink] = useState("");
+  const [loading, setLoading] = useState(false);
 
   const handleDonate = async () => {
-    const tx = await donate(amount, message, link);
+    try {
+      setLoading(true);
 
-    await fetch("http://localhost:5000/api/donations", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({
-        donorAddress: window.ethereum.selectedAddress,
-        amount,
-        message,
-        mediaLink: link,
-        txHash: tx.hash,
-      }),
-    });
-    alert("Donation sent!");
+      const tx = await donate(amount, message, link);
+
+      await fetch("http://localhost:5000/api/donations", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+            donorAddress: window.ethereum.selectedAddress,
+            amount,
+            message,
+            mediaLink: link,
+            txHash: tx.hash,
+        }),
+      });
+
+      setAmount("");
+      setMessage("");
+      setLink("");
+
+      alert("Donation sent!");
+    } catch (e) {
+      console.error(e);
+      alert("Donation failed!");
+    }
+    finally {
+      setLoading(false);
+    }
   };
 
   return (
