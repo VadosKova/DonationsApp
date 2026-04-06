eb589d76de51d0c28926934deec1b58af0a22f97
Vadim Kovalyov <vadimkovalyov06@gmail.com>
2026-04-06 14:26:13 +0300

    Updated routes and added abi

--- donate/src/App.jsx
+++ donate/src/App.jsx
@@ -1,5 +1,5 @@
-import DonateForm from "../components/DonateForm";
-import DonationList from "../components/DonationList";
+import DonateForm from "./components/DonateForm";
+import DonationList from "./components/DonationList";
 import './App.css'
 
 export default function App() {
--- donate/src/web3/contract.js
+++ donate/src/web3/contract.js
@@ -1,7 +1,7 @@
 import { ethers } from "ethers";
 import abi from "./abi.json";
 
-const contractAddress = "address";
+const contractAddress = "0x5e9BDFa6DE23E1ECC94cF64f94aF4c353Bb59cd1";
 
 export const donate = async (amount, message, mediaLink) => {
   const provider = new ethers.BrowserProvider(window.ethereum);
